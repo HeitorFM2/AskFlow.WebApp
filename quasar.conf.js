1ab9d83a00ef881816552013c23a21e7ea3c94a3
HeitorFM2 <heitorfm3@gmail.com>
2026-05-25 22:50:03 -0300

    chore: change vueRouterMode

--- quasar.conf.js
+++ quasar.conf.js
@@ -43,7 +43,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: "history", // available values: 'hash', 'history'
+      vueRouterMode: "hash", // available values: 'hash', 'history'
       env: {
         VUE_APP_API: process.env.VUE_APP_API,
       },
